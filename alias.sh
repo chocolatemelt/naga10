a52a450e788a3265e4029232f4b3fc0f75e3d162
Kevin Zhang <kzhaaang@gmail.com>
2017-03-29 15:50:50 -0400

    add output

--- alias.sh
+++ alias.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/sh
 #hacky way to update fonts.alias
 
-cp /usr/share/fonts/misc/fonts.alias /usr/share/fonts/misc/fonts.alias.old
+echo "creating /usr/share/fonts/misc/fonts.alias backup..."
+cp /usr/share/fonts/misc/fonts.alias /usr/share/fonts/misc/fonts.alias.old && echo "backup @ /usr/share/fonts/misc/fonts.alias.old"
 
 new=false
 
+echo "aliasing from local fonts.alias..."
 while read f; do
 	if ! grep -q -F "$f" /usr/share/fonts/misc/fonts.alias ; then
 		if ! $new ; then
@@ -14,3 +16,4 @@ while read f; do
 		echo "$f" >> /usr/share/fonts/misc/fonts.alias
 	fi
 done < fonts.alias
+echo "done."
